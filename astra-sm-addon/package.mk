5f6d6a2834019eb40e5e993199d9a90d6c187686
roisiano <63460832+roisiano@users.noreply.github.com>
2026-02-24 14:40:30 +0100

    Create package.mk

---
+++ astra-sm-addon/package.mk
@@ -0,0 +1,43 @@
+PKG_NAME="astra-sm"
+PKG_VERSION="master"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://gitlab.com/berdyansk/astra-sm"
+PKG_URL="https://gitlab.com/berdyansk/astra-sm/-/archive/${PKG_VERSION}/astra-sm-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="service"
+PKG_SHORTDESC="astra-sm: lightweight DVB/TS streaming engine"
+PKG_LONGDESC="astra-sm is a lightweight streaming engine for DVB and TS processing."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="astra-sm"
+PKG_ADDON_TYPE="xbmc.service"
+
+addon() {
+  mkdir -p ${ADDON_BUILD}/bin
+  cp ${PKG_BUILD}/src/astra-sm ${ADDON_BUILD}/bin/
+
+  mkdir -p ${ADDON_BUILD}/config
+  mkdir -p ${ADDON_BUILD}/service
+  mkdir -p ${ADDON_BUILD}/resources
+
+  cp ${PKG_DIR}/addon.xml ${ADDON_BUILD}/
+  cp ${PKG_DIR}/service.sh ${ADDON_BUILD}/service/
+  cp ${PKG_DIR}/resources/settings.xml ${ADDON_BUILD}/resources/
+}
+
+configure_target() {
+  ./autogen.sh
+  ./configure \
+    --host=${TARGET_NAME} \
+    --prefix=/usr \
+    --without-check \
+    --without-ffmpeg \
+    --without-libaio \
+    --without-lua
+}
+
+makeinstall_target() {
+  :
+}
